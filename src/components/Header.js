ad820f7d891321abdf6162500af8b0c462de4161
Figueras <maxime.figueras@gmail.com>
2021-02-03 20:57:05 +0100

    [Fix] quizz p in menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import logo from '../assets/img/fi-sr-apps.svg'
 const Header = () => {
     return (
         <header className="header">
-            <p>Quizz</p>
             <img src={logo}/>
         </header>
     )
