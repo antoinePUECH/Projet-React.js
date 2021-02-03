65bc1364a82d7fc14a2502d2276f84cab85fe949
Antoine Puech <bitos@macbook-pro-de-antoine.home>
2021-02-03 16:24:40 +0100

    create json file for quizz ad create component of first quizz

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../sass/components/_header.scss';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <p>Quizz</p>
+        </header>
+    )
+}
+
+export default Header
