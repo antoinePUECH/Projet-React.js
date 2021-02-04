d5604ea0ff513c51bd5b2bc3704d6f26c57403df
Figueras <maxime.figueras@gmail.com>
2021-02-04 17:44:15 +0100

    [Add]Sticky menu change scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
+import React, {useState, useEffect} from 'react';
+
 import '../sass/components/_header.scss';
 import logo from '../assets/img/fi-sr-apps.svg'
 
-const Header = () => {
+function Header() {
+
+    const [scroll, setScroll] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            setScroll(window.scrollY > 10)
+        });
+    }, [])
+
     return (
-        <header className="header">
+        <header role="banner" className={scroll ? "header-scroll" : "header"}>
             <img src={logo} alt="logo"/>
         </header>
     )
