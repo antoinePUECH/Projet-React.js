9dc833eccb0b86dba128d75c9487728be612584e
Figueras <maxime.figueras@gmail.com>
2021-02-06 14:02:18 +0100

    [Add] Props CTA quizz and Quizz component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './sass/_app.scss';
-import Header from './components/Header'
-import Quizz1 from './components/Quizz1'
-import Title from './components/Title'
-import Landing from './components/Landing'
+import Header from './components/Header';
+import Quizz1 from './components/Quizz1';
+import Quizzs from './components/Quizzs';
+import Landing from './components/Landing';
 import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
    <React.Fragment>
+     <Header/>
     <Switch>
     <Route exact path="/">
         <Landing></Landing>
       </Route>
       <Route exact path="/quizzs">
-        <Header/>
-        <Title txt="NOS QUIZZ"/>
+        <Quizzs></Quizzs>
       </Route>
       <Route exact path="/quizz1">
         <Quizz1></Quizz1>
