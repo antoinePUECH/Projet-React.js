4075700a16f17691ed07e4139408bffccc107746
Antoine Puech <bitos@macbook-pro-de-antoine.home>
2021-02-07 15:22:34 +0100

    install jQuery, create function to get API of weather and show temp of city searching by users

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-
+import $ from 'jquery'
 import '../sass/components/_header.scss';
 import logo from '../assets/img/fi-sr-apps.svg'
 import CtaQuizz from '../components/CtaQuizz';
@@ -14,6 +14,26 @@ function Header() {
         });
     }, [])
 
+    var showData = function(data) {
+        console.log("donnees api", data)
+        //alert("Meteo temp  : "  + data.main.temp);
+        var element = document.getElementById("zone_meteo");
+        element.innerHTML = "La temperature est de " + data.main.temp + "°C";
+    }
+    function getWeatherAPI() {
+        var search = document.getElementById("search").value;
+        var url = "https://api.openweathermap.org/data/2.5/weather?q="+search+"&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric"
+        $.get(url, showData).done(function() {
+            //alert( "second success" );
+          })
+          .fail(function() {
+            alert( "Veuillez insérer un nom de ville correcte" );
+          })
+          .always(function() {
+            //alert( "finished" );
+          });
+    }
+
     return (
         <header role="banner" className={scroll ? "header-scroll" : "header"}>
             <div className="logo">
@@ -21,6 +41,9 @@ function Header() {
                     <img src={logo} alt="logo"/>
                 </Link>
             </div>
+            <button onClick= {() => getWeatherAPI()}>Cherchez</button>
+            <input type="text" id="search"></input>
+            <p id="zone_meteo"></p>
             <CtaQuizz href="/quizzs" txt="Nos quizz"></CtaQuizz>
         </header>
     )
