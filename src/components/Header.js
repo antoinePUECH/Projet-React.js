aee00f16e10d2c8d72327144a62f41ae8ae7369a
Antoine Puech <antoine.puech@edu.devinci.fr>
2021-11-14 12:02:56 +0100

    test cd ci

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+
+import '../sass/components/_header.scss';
+import logo from '../assets/img/fi-sr-apps.svg'
+import CtaQuizz from '../components/CtaQuizz';
+import {Link} from 'react-router-dom';
+
+function Header() {
+
+    const [scroll, setScroll] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            setScroll(window.scrollY > 10)
+        });
+    }, [])
+
+    return (
+        <header role="banner" className={scroll ? "header-scroll" : "header"}>
+            <div className="logo">
+                <Link to="/">
+                    <img src={logo} alt="logo"/>
+                </Link>
+            </div>
+            <CtaQuizz href="/quizzs" txt="Nos quizz"></CtaQuizz>
+        </header>
+    )
+}
+
+export default Header
