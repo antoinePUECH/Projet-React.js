9108051a42bec062d3636984d6afed21cf9be291
Antoine Puech <antoine.puech@edu.devinci.fr>
2021-11-14 14:03:47 +0100

    run eslint cd ci

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -12,7 +12,7 @@ var callBackGetSuccess = function(data) {
 function buttonClickGET() {
     var url = "https://api.openweathermap.org/data/2.5/weather?q=Paris,fr&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric"
 
-    $.get(url, callBackGetSuccess).done(function() {
+    get(url, callBackGetSuccess).done(function() {
         //alert( "second success" );
       })
       .fail(function() {
