afcbea716002ab945109f83f403e4df6d7b18990
Antoine Puech <antoine.puech@edu.devinci.fr>
2021-11-14 20:26:00 +0100

    deploy heroku  cd ci

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-import '../sass/components/_header.scss';
-import logo from '../assets/img/fi-sr-apps.svg'
-import CtaQuizz from '../components/CtaQuizz';
-import {Link} from 'react-router-dom';
+import "../sass/components/_header.scss";
+import logo from "../assets/img/fi-sr-apps.svg";
+import CtaQuizz from "../components/CtaQuizz";
+import { Link, Router } from "react-router-dom";
 
 function Header() {
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 10);
+    });
+  }, []);
 
-    const [scroll, setScroll] = useState(false);
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            setScroll(window.scrollY > 10)
-        });
-    }, [])
-
-    return (
-        <header role="banner" className={scroll ? "header-scroll" : "header"}>
-            <div className="logo">
-                <Link to="/">
-                    <img src={logo} alt="logo"/>
-                </Link>
-            </div>
-            <CtaQuizz href="/quizzs" txt="Nos quizz"></CtaQuizz>
-        </header>
-    )
+  return (
+    <header role="banner" className={scroll ? "header-scroll" : "header"}>
+      <div className="logo">
+        <Router>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </Router>
+      </div>
+      <CtaQuizz href="/quizzs" txt="Nos quizz"></CtaQuizz>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/atoms/QuizzBlock.js
+++ src/components/atoms/QuizzBlock.js
@@ -1,20 +1,22 @@
-import { Link } from 'react-router-dom';
-import '../../sass/components/_quizzbloc.scss';
+import { Link, Router } from "react-router-dom";
+import "../../sass/components/_quizzbloc.scss";
 
 const QuizzBlock = (props) => {
-    return(
-        <div className="quizzBloc">
-            <Link to={props.to}>
-                <div className="fond">
-                    <img src={props.img} alt="imgQuizz"/>
-                </div>
-                <div className="filter">
-                    <p>QUIZZ</p>
-                    <h2>{props.titleQuizz}</h2>
-                </div>
-            </Link>
-        </div>
-    )
-}
+  return (
+    <div className="quizzBloc">
+      <Router>
+        <Link to={props.to}>
+          <div className="fond">
+            <img src={props.img} alt="imgQuizz" />
+          </div>
+          <div className="filter">
+            <p>QUIZZ</p>
+            <h2>{props.titleQuizz}</h2>
+          </div>
+        </Link>
+      </Router>
+    </div>
+  );
+};
 
-export default QuizzBlock
+export default QuizzBlock;
